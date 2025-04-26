a6d4a2001b53c5c8d3ac45e46b6a691d2872e238
fbin <ntbinh243.dev@gmail.com>
2025-04-27 00:41:34 +0700

    0028. Find the Index of the First Occurrence in a String

---
+++ 0000-0999/0028. Find the Index of the First Occurrence in a String/Solution.cpp
@@ -0,0 +1,30 @@
+/**
+ * Author: Fbin
+ * Link:https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/?envType=study-plan-v2&envId=top-interview-150
+ * Created at: 27/04/2025
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+ public:
+  int strStr(string haystack, string needle) {
+    int i = 0, j = 0;
+    int n = haystack.length();
+    int m = needle.length();
+    while (i < n) {
+      j = 0;
+      int start = i;
+      while (start < n && j < m && needle[j] == haystack[start]) {
+        start++;
+        j++;
+      }
+
+      if (j == m) return start - m;
+      i++;
+    }
+
+    return -1;
+  }
+};
