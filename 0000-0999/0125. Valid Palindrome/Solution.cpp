36fed00264c24b8aac46415a5964c8f01050180f
fbin <ntbinh243.dev@gmail.com>
2025-04-27 13:44:05 +0700

    0125. Valid Palindrome

---
+++ 0000-0999/0125. Valid Palindrome/Solution.cpp
@@ -0,0 +1,33 @@
+/**
+ * Author: Fbin
+ * Link:https://leetcode.com/problems/valid-palindrome/description/?envType=study-plan-v2&envId=top-interview-150
+ * Created at: 27/04/2025
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+ public:
+  bool isPalindrome(string s) {
+    int i = 0;
+    int j = s.length() - 1;
+    while (i <= j) {
+      if (!isalnum(s[i])) {
+        i++;
+        continue;
+      }
+      if (!isalnum(s[j])) {
+        j--;
+        continue;
+      }
+      if (tolower(s[i]) == tolower(s[j])) {
+        i++;
+        j--;
+      } else
+        return false;
+    }
+
+    return true;
+  }
+};
