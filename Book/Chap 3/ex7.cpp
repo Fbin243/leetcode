58c0ef9023127e5e3ec5e3a43c7e7ff0385c6561
fbin <ntbinh243.dev@gmail.com>
2025-05-12 17:41:03 +0700

    ex 7 remove duplicates in linked list

---
+++ Book/Chap 3/ex7.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+  int val;
+  Node* next;
+};
+
+void InsertNode(Node*& head, Node*& tail, int val) {
+  Node* newNode = new Node{val, nullptr};
+  if (!head) head = tail = newNode;
+  tail->next = newNode;
+  tail = newNode;
+}
+
+void Print(Node* head) {
+  while (head) {
+    cout << head->val << " ";
+    head = head->next;
+  }
+  cout << endl;
+}
+
+void Destroy(Node*& head, Node*& tail) {
+  while (head) {
+    Node* temp = head;
+    head = head->next;
+    delete temp;
+  }
+
+  tail = nullptr;
+}
+
+Node* removeDupes(Node* head) {
+  Node* cur = head->next;
+  Node* prev = head;
+  while (cur) {
+    if (cur->val == prev->val) {
+      Node* temp = cur;
+      prev->next = cur->next;
+      cur = cur->next;
+      delete temp;
+    } else {
+      prev = cur;
+      cur = cur->next;
+    }
+  }
+
+  return head;
+}
+
+int main() {
+  Node *head = nullptr, *tail = nullptr;
+  srand(time(NULL));
+  for (int i = 0; i < 5; i++) {
+    for (int j = 0; j < rand() % 10; j++) {
+      InsertNode(head, tail, i);
+    }
+  }
+
+  cout << "Before remove duplicates: ";
+  Print(head);
+
+  head = removeDupes(head);
+
+  cout << "After remove duplicates: ";
+  Print(head);
+  Destroy(head, tail);
+
+  return 0;
+}
