27427e25e88a6794b3e5bf1a7a1f5f3bac15a0f4
fbin <ntbinh243.dev@gmail.com>
2025-05-23 10:49:38 +0700

    0167. Two Sum II - Input Array Is Sorted

--- 0000-0999/0167. Two Sum II - Input Array Is Sorted/Solution.cpp
+++ 0000-0999/0167. Two Sum II - Input Array Is Sorted/Solution.cpp
@@ -44,3 +44,22 @@ class Solution {
     return vector<int>{-1, -1};
   }
 };
+
+// --- Solution at 23/05/2025
+class Solution {
+ public:
+  vector<int> twoSum(vector<int>& numbers, int target) {
+    int i = 0, j = numbers.size() - 1;
+    while (i < j) {
+      int sum = numbers[i] + numbers[j];
+      if (sum == target)
+        return vector<int>{i + 1, j + 1};
+      else if (sum > target)
+        j--;
+      else
+        i++;
+    }
+
+    return vector<int>{0, 0};
+  }
+};
