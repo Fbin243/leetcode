c8f6db1ca297b70333db4d5bb1bc31759dee7922
fbin <ntbinh243.dev@gmail.com>
2025-05-26 11:35:37 +0700

    0153. Find Minimum in Rotated Sorted Array

--- 0000-0999/0153. Find Minimum in Rotated Sorted Array/Solution.cpp
+++ 0000-0999/0153. Find Minimum in Rotated Sorted Array/Solution.cpp
@@ -48,3 +48,23 @@ class Solution {
     return nums[l];
   }
 };
+
+// -- Solution at 26/05/2025
+class Solution {
+ public:
+  int findMin(vector<int>& nums) {
+    int l = 0, r = nums.size() - 1;
+    int res = INT_MAX;
+    while (l <= r) {
+      int m = l + (r - l) / 2;
+      if (nums[m] <= nums[r]) {
+        res = min(res, nums[m]);
+        r = m - 1;
+      } else {
+        l = m + 1;
+      }
+    }
+
+    return res;
+  }
+};
