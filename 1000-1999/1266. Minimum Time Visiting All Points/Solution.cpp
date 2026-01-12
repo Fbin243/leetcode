b433d8c89c36de9019d3726325495d7604c84a56
fbin <ntbinh243.dev@gmail.com>
2026-01-12 21:44:07 +0700

    1266. Minimum Time Visiting All Points

---
+++ 1000-1999/1266. Minimum Time Visiting All Points/Solution.cpp
@@ -0,0 +1,19 @@
+/**
+ * Author: Fbin
+ * Link:https://leetcode.com/problems/minimum-time-visiting-all-points/\?envType\=daily-question\&envId\=2026-01-10
+ * Created at: 12/01/2026
+ */
+
+#include <fbin/leetcode.h>
+
+class Solution {
+ public:
+  int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+    int ans = 0;
+    for (int i = 1; i < points.size(); i++) {
+      ans += max(abs(points[i][0] - points[i - 1][0]),
+                 abs(points[i][1] - points[i - 1][1]));
+    }
+    return ans;
+  }
+};
