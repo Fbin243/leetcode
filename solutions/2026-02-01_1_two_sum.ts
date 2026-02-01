3c9f92e268d115db6637afe037a0e4c07f134b20
fbin <ntbinh243.dev@gmail.com>
2026-02-01 09:27:59 +0700

    feat: Add solution for daily challenge 2026-02-01 - Two Sum

---
+++ solutions/2026-02-01_1_two_sum.ts
@@ -0,0 +1,21 @@
+/**
+ * Two Sum
+ *
+ * Difficulty: Easy
+ * Topics: Array, Hash Table
+ * Date: 2/1/2026
+ *
+ * Problem Description:
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+Example:
+[2,7,11,15]
+9
+ */
+
+function solution() {
+  // TODO: Implement solution here
+
+}
+
+export default solution;
