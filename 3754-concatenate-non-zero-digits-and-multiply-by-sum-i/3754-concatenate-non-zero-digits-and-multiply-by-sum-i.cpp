f23d6ed0ac617a2e8bd98a7abb57747c23a167bb
Binh Nguyen (Fbin) <100687537+Fbin243@users.noreply.github.com>
2026-07-26 14:46:52 +0700

    Time: 0 ms (100%), Space: 9.1 MB (52.4%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -1,22 +1,10 @@
 class Solution {
-#define ll long long int
-
 public:
     long long sumAndMultiply(int n) {
-        ll s = 0, res = 0, m = 1;
-
-        while (n) {
-            int d = n % 10;
-
-            if (d) {
-                res = d * m + res;
-                m *= 10;
-                s += d;
-            }
-
-            n /= 10;
-        }
-
-        return res * s;
+        long long x = 0, s = 0;
+        for (char c : to_string(n))
+            if (c != '0')
+                x = x * 10 + c - '0', s += c - '0';
+        return x * s;
     }
 };
